691e0faad91b48fffb3cbd6534853d73515baac2
Nana2929 <b06102020@ntu.edu.tw>
2022-12-19 12:48:54 +0000

    add: training entry point

--- run_official.sh
+++ run_official.sh
@@ -1,34 +1,26 @@
 #!/bin/bash
 set -e # exit on error
 datadir="Dataset"
-dset="GooglePixel"
-textcol="title"                       # text title
+dset="Laptop"
+finetuned="ft"          # "ft", "no-ft"
+ftptm="roberta-en"      # if fine-tuned, choose from 'bert-en-base-uncased', 'roberta-en', 'roberta-en-large', 'xlmroberta-xlm-roberta-base', 'bert-multi-base-cased', 'xlnet-xlnet-base-cased'
 
-finetuned="no-ft"                       # "ft", "no-ft"
-ftptm="bert-en-base-uncased"        # if fine-tuned, choose from 'bert-en-base-uncased', 'roberta-en', 'roberta-en-large', 'xlmroberta-xlm-roberta-base', 'bert-multi-base-cased', 'xlnet-xlnet-base-cased'
-ptm="bert"                          # otherwise, choose from 'bert', 'roberta', 'xlmroberta', 'xlnet'
-
+ptm="roberta"
 project_root="/home/P76114511/projects/RoBERTaABSA"
-ftdset="Laptop"
-ft_model_path="$project_root/Train/save_models/$ftptm-$ftdset-FT/$ftptm"
 
 # run this script at project root:
 # Note: please activate your venv before running. mb activate robertaabsa
-python3 ./Dataset/csv2json14.py --base_dir="$datadir/$dset" --textcol=$textcol
+ft_model_path="$project_root/Train/save_models/$ftptm-$dset-FT/$ftptm"
+# python3 ./Dataset/csv2json14.py --base_dir="$datadir/$dset" --textcol=$textcol
 if [[ $finetuned == "ft" ]]
 then
     echo == do finetuning ==
-    python3 Train/finetune.py --root_fp=$project_root\
-    --model_name=$ftptm --n_epochs=5 --data_dir=$datadir\
-    --dset_name=$dset\
-    --is_finetuned=$finetuned\
-    --dataset=$ftdset
-    python3 Perturbed-Masking/generate_matrix.py --model_path=$ft_model_path --data_dir=$datadir --dataset=$ftdset
+    python3 Train/finetune.py --root_fp=$project_root --model_name=$ftptm --n_epochs=1 --data_dir=$datadir --dataset=$dset # need to use their dataset
+    python3 Perturbed-Masking/generate_matrix.py --model_path=$ft_model_path --data_dir=$datadir --dataset=$dset
 else
     echo == no finetuning ==
     python3 Perturbed-Masking/generate_matrix.py --model_path=$ptm --data_dir=$datadir --dataset=$dset
 fi
-
 echo == training ALSC ==
 for layer in {0..12}
 do
@@ -38,3 +30,6 @@ do
     python3 ASGCN/train.py --dataset=asgcn2/$ptm/$layer/$dset \
     --is_finetuned=$finetuned --ptm_name=$ptm --root_fp=$project_root --layers=$layer
 done;
+
+
+
