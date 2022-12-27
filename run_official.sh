2fbc059e91c9f1fb765a3f134c9258217507661e
Nana2929 <b06102020@ntu.edu.tw>
2022-12-27 03:28:11 +0000

    mod

--- run_official.sh
+++ run_official.sh
@@ -11,23 +11,23 @@ echo == run_official.sh ==
 # run this script at project root:
 # Note: please activate your venv before running. mb activate robertaabsa
 ft_model_path="$project_root/Train/save_models/$ftptm-$dset-FT/$ftptm"
-if [[ $finetuned == "ft" ]]
-then
-    echo == do finetuning ==
-    python3 Train/finetune.py --root_fp=$project_root --model_name=$ftptm --n_epochs=1 --data_dir=$datadir --dataset=$dset # need to use their dataset
-    python3 Perturbed-Masking/generate_matrix.py --model_path=$ft_model_path --data_dir=$datadir --dataset=$dset
-else
-    echo == no finetuning ==
-    python3 Perturbed-Masking/generate_matrix.py --model_path=$ptm --data_dir=$datadir --dataset=$dset
-fi
+# if [[ $finetuned == "ft" ]]
+# then
+#     echo == do finetuning ==
+#     python3 Train/finetune.py --root_fp=$project_root --model_name=$ftptm --n_epochs=1 --data_dir=$datadir --dataset=$dset # need to use their dataset
+#     python3 Perturbed-Masking/generate_matrix.py --model_path=$ft_model_path --data_dir=$datadir --dataset=$dset
+# else
+#     echo == no finetuning ==
+#     python3 Perturbed-Masking/generate_matrix.py --model_path=$ptm --data_dir=$datadir --dataset=$dset
+# fi
 
 echo == training ALSC ==
-for layer in {0..12}
+for layer in 7
 do
     echo == training w/ $layer layer ==
     python3 Perturbed-Masking/generate_asgcn.py --layer=$layer --is_finetuned=$finetuned --matrix_folder="$ptm/$dset/Train" --root_fp=$project_root
     python3 Perturbed-Masking/generate_asgcn.py --layer=$layer --is_finetuned=$finetuned --matrix_folder="$ptm/$dset/Test"  --root_fp=$project_root
-    python3 ASGCN/train.py --dataset=asgcn2/$ptm/$layer/$dset \
+    python3 ASGCN/train.py --dataset=asgcn2/$ptm/$layer/$dset --save \
     --is_finetuned=$finetuned --ptm_name=$ptm --root_fp=$project_root --layers=$layer
 done
 
